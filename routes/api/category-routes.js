aca0fe102c3174abc8746d0b378f3aaff17662b3
Mackenzie Gray <mdkulen@gmail.com>
2022-08-12 16:38:25 +1000

    changes err repsonse code in routes for PUT request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -71,7 +71,7 @@ router.put('/:id', async (req, res) => {
     res.status(200).json(categoryData);  
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
